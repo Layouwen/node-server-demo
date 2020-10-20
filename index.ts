f0eb2a419b3d7ab3e4b12864833bdc3c89ae848e
liangyouwen <layouwen@gmail.com>
2020-10-20 20:40:18 +0800

    default index url

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
-import * as http from "http";
-import {IncomingMessage, ServerResponse} from "http";
-import * as fs from "fs";
-import * as p from "path";
+import * as http from 'http';
+import {IncomingMessage, ServerResponse} from 'http';
+import * as fs from 'fs';
+import * as p from 'path';
 import * as url from 'url';
 
 const server = http.createServer();
@@ -10,14 +10,16 @@ const publicDir = p.resolve(__dirname, 'public');
 server.on('request', (request: IncomingMessage, response: ServerResponse) => {
   const {method, url: path, headers} = request;
   const {pathname, search} = url.parse(path);
-  const filename = pathname.substr(1)
+  let filename = pathname.substr(1);
+
+  if (filename === '') filename = 'index.html';
   fs.readFile(p.resolve(publicDir, filename), (error, data) => {
     if (error) {
-      console.log(error)
-      response.statusCode = 404
-      response.end()
-    }else{
-      response.end(data.toString())
+      fs.readFile(p.resolve(publicDir, '404.html'), (error, data) => {
+        response.end(data);
+      });
+    } else {
+      response.end(data);
     }
   });
 });
