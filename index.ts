6ae5c4ed53f28c64e230d8bc32d53412317fbe0f
layouwen <layouwen@gmail.com>
2020-10-20 18:44:51 +0800

    update url index.ts

--- index.ts
+++ index.ts
@@ -1,22 +1,37 @@
 import * as http from "http";
 import {IncomingMessage, ServerResponse} from "http";
+import * as fs from "fs";
+import * as p from "path";
 
 const server = http.createServer();
+const publicDir = p.resolve(__dirname, 'public');
 
 server.on('request', (request: IncomingMessage, response: ServerResponse) => {
-  const array = [];
-  console.log(request.httpVersion);
-  console.log(request.url);
-
-  request.on('data', (chunk) => {
-    array.push(chunk);
-  });
-  request.on('end', () => {
-    const body = Buffer.concat(array).toString();
-    console.log('body', body);
-    response.end('finish')
-  });
-
+  const {method, url, headers} = request;
+  switch (url) {
+    case '/index':
+      fs.readFile(p.resolve(publicDir, 'index.html'), (error, data) => {
+        if (error) throw error;
+        response.end(data.toString());
+      });
+      break;
+    case '/style.css':
+      response.setHeader('Content-Type', 'text/css; charset=utf-8;')
+      fs.readFile(p.resolve(publicDir, 'style.css'), (error, data) => {
+        if (error) throw error;
+        response.end(data.toString());
+      });
+      break;
+    case '/main.js':
+      response.setHeader('Content-Type', 'text/javascript; charset=utf-8;')
+      fs.readFile(p.resolve(publicDir, 'main.js'), (error, data) => {
+        if (error) throw error;
+        response.end(data.toString());
+      });
+      break;
+    default:
+      response.end();
+  }
 });
 
 server.listen(8888);
