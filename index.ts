e1ef50b7f4635672fa12b507212750a28ba1a926
liangyouwen <layouwen@gmail.com>
2020-10-20 21:09:28 +0800

    ignore requests outside GET

--- index.ts
+++ index.ts
@@ -10,15 +10,21 @@ const publicDir = p.resolve(__dirname, 'public');
 server.on('request', (request: IncomingMessage, response: ServerResponse) => {
   const {method, url: path, headers} = request;
   const {pathname, search} = url.parse(path);
+
+  if (method !== 'GET') {
+    response.statusCode = 405;
+    response.end();
+    return;
+  }
   let filename = pathname.substr(1);
 
   if (filename === '') filename = 'index.html';
   fs.readFile(p.resolve(publicDir, filename), (error, data) => {
     if (error) {
-      fs.readFile(p.resolve(publicDir, '404.html'), (error, data) => {
-        response.end(data);
-      });
+      response.statusCode = 404;
+      response.end();
     } else {
+      response.setHeader('Cache-Control', 'public, max-age=1024');
       response.end(data);
     }
   });
